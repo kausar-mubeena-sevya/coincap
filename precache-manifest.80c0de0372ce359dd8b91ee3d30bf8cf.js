2695a12e40cbbac9aed900dc5398bca7b8cf352a
Kausar <kausar.shaik@sevyait.com>
2020-09-04 12:03:26 +0530

    Updates

--- precache-manifest.80c0de0372ce359dd8b91ee3d30bf8cf.js
+++ precache-manifest.80c0de0372ce359dd8b91ee3d30bf8cf.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2cae77e75adbaca7f815267e4c826bc5",
+    "revision": "64f50340536e85cec30ea6c84e9fcbac",
     "url": "/coincap/index.html"
   },
   {
-    "revision": "5f356ab6c98cea055c78",
-    "url": "/coincap/static/css/main.d04388d7.chunk.css"
+    "revision": "0282223740baf6c46956",
+    "url": "/coincap/static/css/main.5c89f017.chunk.css"
   },
   {
     "revision": "eb33f1c25e51ed6e561f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/coincap/static/js/2.9213ffb8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5f356ab6c98cea055c78",
-    "url": "/coincap/static/js/main.a2b1ac84.chunk.js"
+    "revision": "0282223740baf6c46956",
+    "url": "/coincap/static/js/main.1c4e69a6.chunk.js"
   },
   {
     "revision": "b463c5a5f314404573ac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coincap/precache-manifest.765487767e637415184273e52e351b8b.js"
+  "/coincap/precache-manifest.80c0de0372ce359dd8b91ee3d30bf8cf.js"
 );
 
 self.addEventListener('message', (event) => {
