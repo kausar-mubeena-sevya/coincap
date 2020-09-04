3793f1a47ac57a9cb4bad7ca295a9e2ffd7124c3
Kausar <kausar.shaik@sevyait.com>
2020-09-04 11:41:11 +0530

    Updates

---
+++ precache-manifest.a91d887884969613c0dd541b96f45693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d389ef3578db90988899d9a5e28565",
+    "url": "/coincap/index.html"
+  },
+  {
+    "revision": "9eb5966b3069277e6422",
+    "url": "/coincap/static/css/main.d04388d7.chunk.css"
+  },
+  {
+    "revision": "eb33f1c25e51ed6e561f",
+    "url": "/coincap/static/js/2.9213ffb8.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/coincap/static/js/2.9213ffb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eb5966b3069277e6422",
+    "url": "/coincap/static/js/main.ce6093ba.chunk.js"
+  },
+  {
+    "revision": "b463c5a5f314404573ac",
+    "url": "/coincap/static/js/runtime-main.31922bda.js"
+  }
+]);
